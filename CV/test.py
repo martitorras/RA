350238c5e5ed0d9d924b26a3871afa8a1d6c566e
Martí Torras Isanta <martiti@TR12.CAT>
2020-02-12 13:56:14 +0100

    video

--- CV/test.py
+++ CV/test.py
@@ -1,2 +1,24 @@
-a = 1
-print("Minimum (1,2) = "+str(a))
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        #frame = cv2.flip(frame,0)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+        out.write(frame)
+
+        cv2.imshow('Video Frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
